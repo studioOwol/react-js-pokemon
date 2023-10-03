3f1ac16632d6d8a1d4ff07a3eabed1493b4d4061
Nahyun <from.owol@gmail.com>
2023-10-03 21:38:58 +0900

    feat: create pokemon cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import axios from 'axios';
+import PokeCard from './components/PokeCard';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0';
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
 
   useEffect(() => {
     fetchPokeData();
@@ -30,7 +31,9 @@ function App() {
       <section className='pt-6 flex flex-col justify-center items-center overflow-auto z-0'>
         <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
           {pokemons.length > 0 ? (
-            pokemons.map(({ url, name }, index) => <div>{name}</div>)
+            pokemons.map(({ url, name }, index) => (
+              <PokeCard key={url} url={url} name={name} />
+            ))
           ) : (
             <h2 className='font-medium text-lg text-slate-900 mb-1'>
               포켓몬이 없습니다.
