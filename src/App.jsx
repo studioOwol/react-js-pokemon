5e9c484a6730c5b24e314722d753bd462380b682
Nahyun <from.owol@gmail.com>
2023-10-03 20:41:41 +0900

    feat: get data from poke api

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0';
+
+  useEffect(() => {
+    fetchPokeData();
+  }, []);
+
+  const fetchPokeData = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response.data.results);
+      setPokemons(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <article className='pt-6'>
+      <header className='flex flex-col gap-2 w-full px-4 z-50'>
+        Input Form
+      </header>
+      <section className='pt-6 flex flex-col justify-center items-center overflow-auto z-0'>
+        <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
+          {pokemons.length > 0 ? (
+            pokemons.map(({ url, name }, index) => <div>{name}</div>)
+          ) : (
+            <h2 className='font-medium text-lg text-slate-900 mb-1'>
+              포켓몬이 없습니다.
+            </h2>
+          )}
+        </div>
+      </section>
+    </article>
+  );
+}
+
+export default App;
