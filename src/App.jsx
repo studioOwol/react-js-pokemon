bea531b87b791c183b5f890ff5180d505f1d0d0c
Nahyun <from.owol@gmail.com>
2023-10-04 19:54:22 +0900

    feat: add read more button

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ import PokeCard from './components/PokeCard';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
 
   useEffect(() => {
-    fetchPokeData();
+    fetchPokeData(true);
   }, []);
 
-  const fetchPokeData = async () => {
+  const fetchPokeData = async (isFirstFetch) => {
     try {
+      const offsetValue = isFirstFetch ? 0 : offset + limit;
+      const url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offsetValue}`;
       const response = await axios.get(url);
-      console.log(response.data.results);
-      setPokemons(response.data.results);
+      // console.log(response.data.results);
+      setPokemons([...pokemons, ...response.data.results]);
+      setOffset(offsetValue);
     } catch (error) {
       console.error(error);
     }
@@ -41,6 +45,15 @@ function App() {
           )}
         </div>
       </section>
+
+      <div className='text-center'>
+        <button
+          onClick={() => fetchPokeData(false)}
+          className='bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white'
+        >
+          더 보기
+        </button>
+      </div>
     </article>
   );
 }
