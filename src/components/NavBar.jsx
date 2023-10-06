0779966650026f12473ff359702d4e3a59298140
Nahyun <from.owol@gmail.com>
2023-10-06 21:50:32 +0900

    feat: add navbar ui with styled component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [show, setShow] = useState(false);
+
+  const listener = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listener);
+
+    return () => {
+      window.removeEventListener('scroll', listener);
+    };
+  }, []);
+
+  return (
+    <NavWrapper show={show}>
+      <Logo>
+        <Image
+          alt='Poke Logo'
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'
+          onClick={() => (window.location.href = '/')}
+        />
+      </Logo>
+    </NavWrapper>
+  );
 };
+
+const Image = styled.img`
+  cursor: pointer;
+  width: 100%;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 50px;
+  margin-top: 4px;
+`;
+
+const NavWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  display: flex;
+  background-color: ${(props) => (props.show ? '#090b13' : 'transparent')};
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 100;
+`;
 
 export default NavBar;
